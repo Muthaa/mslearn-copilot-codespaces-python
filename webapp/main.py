d88dd4361d5954458b1c323042c4170ee87d90be
Philip Kyalo <32406905+Muthaa@users.noreply.github.com>
2024-06-07 12:50:39 +0000

    generate a Pydantic model

--- webapp/main.py
+++ webapp/main.py
@@ -27,7 +27,8 @@ def root():
 @app.post('/generate')
 def generate(body: Body):
     """
-    Generate a pseudo-random token ID of twenty characters by default. Example POST request body:
+    Generate a pseudo-random token ID of twenty characters by default
+      Example POST request body:
 
     {
         "length": 20
@@ -35,3 +36,8 @@ def generate(body: Body):
     """
     string = base64.b64encode(os.urandom(64))[:body.length].decode('utf-8')
     return {'token': string}
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='', port=8000)
